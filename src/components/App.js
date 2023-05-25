08e6650e782cad3ee44ce885c0afd637b4dec140
bouquet1 <bukuleta@gmail.com>
2023-05-25 01:00:26 -0700

    passes test

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
 import React from "react";
+import About from "./About";
+import Home from "./Home";
+import NavBar from "./NavBar";
+import { username, city, image } from "../data/user";
 
 function App() {
   return (
     <div>
+      {(username, city, image)}
       <NavBar />
       <Home />
       <About />
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React from "react";
+import { username, city } from "../data/user";
+import NavBar from "./NavBar";
 
 function Home() {
   return (
     <div id="home">
+      <NavBar />
       <h1>
         {username} is a Web Developer from {city}
       </h1>
     </div>
   );
 }
+
+export default Home;
